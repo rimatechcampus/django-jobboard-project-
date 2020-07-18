74af56bf7c81aa7077c3b149b17694ae74daea70
Julia <riyamtechcampus@gmail.com>
2020-07-19 00:02:25 +0300

    add profile and profile edit in accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,49 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from .models import Profile
+from django.urls import reverse
+
 
 # Create your views here.
-from .forms import SignupForm
+from .forms import SignupForm, UserForm, ProfileForm
 
 
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
-            # delete the session after signup
-            username = form.cleaned_data('username')
-            password = form.cleaned_data('password1')
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
             login(request, user)
             return redirect('/accounts/profile')
-
-    form = SignupForm()
+    else:
+        form = SignupForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+
+def profile(request):
+    profile = Profile.objects.get(user=request.user)
+    return render(request, 'accounts/profile.html', {'profile': profile})
+
+
+def profile_edit(request):
+    profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        userform = UserForm(request.POST, instance=request.user)
+        profileform = ProfileForm(
+            request.POST, request.FILES, instance=profile)
+        if userform.is_valid() and profileform.is_valid():
+            userform.save()
+            myprofile = profileform.save(commit=False)
+            myprofile.user = request.user
+            myprofile.save()
+            return redirect(reverse('accounts:profile'))
+
+    else:
+        userform = UserForm(instance=request.user)
+        profileform = ProfileForm(instance=profile)
+
+    return render(request, 'accounts/profile_edit.html', {'userform': userform, 'profileform': profileform})
