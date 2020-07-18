f1d04eed0a375f34bf1f9b58ed4d0d490caaa6d4
Julia <riyamtechcampus@gmail.com>
2020-07-18 22:26:27 +0300

    config accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+from .forms import SignupForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # delete the session after signup
+            username = form.cleaned_data('username')
+            password = form.cleaned_data('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/accounts/profile')
+
+    form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
